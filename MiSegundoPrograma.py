592057c87c31df052ab7fd264959ce47e7ba59ed
José Manuel Gómez <34304848+jmgomezsoriano@users.noreply.github.com>
2019-04-04 23:30:24 +0200

    Subida del segundo ejemplo para el curso MOOC
    Subida del ejemplo para el sexto vídeo del curso MOOC

---
+++ MiSegundoPrograma.py
@@ -0,0 +1,6 @@
+name = input("Por favor, introduce tu nombre: ")
+print("Hola " + name)
+num1 = int(input("Introduce un número: "))
+num2 = int(input("Introduce otro número: "))
+result = num1 * num2
+print("El resultado es: ", result)
