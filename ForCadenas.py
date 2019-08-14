e4a22ce7c3b320fab676384029d2618817fa49fd
José Manuel Gómez <34304848+jmgomezsoriano@users.noreply.github.com>
2019-08-14 22:55:40 +0200

    Adding forcadenas.py

---
+++ ForCadenas.py
@@ -0,0 +1,15 @@
+for c in "torpedo":
+    print(c, end=' ')
+print()
+
+for c in "torpedo fuera":
+    if c == 'd':
+        break
+    print(c, end='')
+print()
+
+for c in "torpedo fuera":
+    if c == 'r':
+        continue
+    print(c, end='')
+print()
